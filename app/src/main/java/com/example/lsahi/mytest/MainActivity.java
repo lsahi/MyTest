8d59f535c0cae4753d708256232de3ac02aacc5b
lsahi <30521187+lsahi@users.noreply.github.com>
2018-03-26 16:41:19 +0800

    added my wife

--- app/src/main/java/com/example/lsahi/mytest/MainActivity.java
+++ app/src/main/java/com/example/lsahi/mytest/MainActivity.java
@@ -4,19 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private EditText editText;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button1=(Button) findViewById(R.id.button_1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        editText =(EditText) findViewById(R.id.edit_text);
+        imageView=(ImageView)findViewById(R.id.image_view);
+        button1.setOnClickListener(this);/*(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 //
             }
-        });
+        });*/
+    }
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button_1:
+                imageView.setImageResource(R.drawable.img_2);
+                break;
+            default:
+                break;
+        }
     }
 }

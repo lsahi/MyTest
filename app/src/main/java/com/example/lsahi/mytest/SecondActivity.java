9748cc003fd012a2b98ab8685287885e0bd5b26f
lsahi <30521187+lsahi@users.noreply.github.com>
2018-03-28 11:50:47 +0800

    add a listview

--- app/src/main/java/com/example/lsahi/mytest/SecondActivity.java
+++ app/src/main/java/com/example/lsahi/mytest/SecondActivity.java
@@ -2,12 +2,20 @@ package com.example.lsahi.mytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private  String[] data={"apple","banana","1","2","cherry","strawberry","234","564664","orange","pear","wut","space","test","man","ls","what","kancolle",};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(
+                SecondActivity.this, android.R.layout.simple_list_item_1,data
+        );
+        ListView listView=(ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }

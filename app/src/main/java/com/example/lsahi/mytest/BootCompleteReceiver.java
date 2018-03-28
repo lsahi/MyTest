5cdeb84123f5de7ad91c220467e8c0c17342304f
lsahi <30521187+lsahi@users.noreply.github.com>
2018-03-28 20:23:20 +0800

    add broadcast and

---
+++ app/src/main/java/com/example/lsahi/mytest/BootCompleteReceiver.java
@@ -0,0 +1,17 @@
+package com.example.lsahi.mytest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context,"BOOT COMPLETE",Toast.LENGTH_LONG).show();
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

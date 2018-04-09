e778f2fcda8c7b1e0ec1032982d730822fc313a7
lsahi <30521187+lsahi@users.noreply.github.com>
2018-04-09 15:27:26 +0800

    Fixed nofitication bug
    in build.gradle
    minSdkVersion15 is too old
    -> fixed int o16

--- app/src/main/java/com/example/lsahi/mytest/MainActivity.java
+++ app/src/main/java/com/example/lsahi/mytest/MainActivity.java
@@ -25,12 +25,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button2=(Button) findViewById(R.id.button_2);
         Button button3=(Button) findViewById(R.id.button_3);
         Button button4=(Button) findViewById(R.id.button_4);
+        Button button5=(Button) findViewById(R.id.button_5);
         editText =(EditText) findViewById(R.id.edit_text);
         imageView=(ImageView)findViewById(R.id.image_view);
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
         /*button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -82,6 +84,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_4:
                 Intent intent4=new Intent(MainActivity.this,MessageActivity.class);
                 startActivity(intent4);
+                break;
+            case R.id.button_5:
+                Intent intent5=new Intent(MainActivity.this,NotificationTest.class);
+                startActivity(intent5);
                 break;
             default:
                 break;//
--- app/src/main/java/com/example/lsahi/mytest/NotificationTest.java
+++ app/src/main/java/com/example/lsahi/mytest/NotificationTest.java
@@ -2,6 +2,7 @@ package com.example.lsahi.mytest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.support.v4.media.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -23,14 +24,15 @@ public class NotificationTest extends AppCompatActivity implements View.OnClickL
     public void onClick(View v){
         switch(v.getId()){
             case R.id.send_notice:
-                NotificationManager manager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                Notification notification =new NotificationCompat.Builder(this)
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notification =new Notification.Builder(this)
                         .setContentTitle("This is content title")
                         .setContentText("This is content text")
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                         .build();
+                //Notification notification=new NotificationCompat.build();
                 manager.notify(1,notification);
                 break;
             default:

8dac7a4b4ec0b8bdce1604a47ab47cea0a2c0f1a
lsahi <lsahi@outlook.com>
2018-05-12 19:45:31 +0800

    set a popup window
    use float icon to pupup window
    edittext

--- app/src/main/java/com/example/lsahi/mytest/ShowSchoolActivity.java
+++ app/src/main/java/com/example/lsahi/mytest/ShowSchoolActivity.java
@@ -1,13 +1,20 @@
 package com.example.lsahi.mytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ShowSchoolActivity extends AppCompatActivity {
 
+    public static final String SCHOOL_NAME="school_name";
+    public static final String SCHOOL_IMAGE_ID="school_image_id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_school);
+        Intent intent=getIntent();
+        String schoolName=intent.getStringExtra(SCHOOL_NAME);
+        //
     }
 }
